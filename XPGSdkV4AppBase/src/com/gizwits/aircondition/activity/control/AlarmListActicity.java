3ad98832b33a8a7bbe584bbd31b95485572c4593
Lien <lli@xtremeprog.com>
2015-01-15 16:11:27 +0800

    test

--- XPGSdkV4AppBase/src/com/gizwits/aircondition/activity/control/AlarmListActicity.java
+++ XPGSdkV4AppBase/src/com/gizwits/aircondition/activity/control/AlarmListActicity.java
@@ -24,6 +24,7 @@ public class AlarmListActicity extends BaseActivity implements OnClickListener {
 	private AlarmListAdapter mAdapter;
 	private ImageView ivBack;
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
